f4606d2f2d38cbbac1d0949c568fe71aaa78c472
waifu4ur <153315802+waifu4ur@users.noreply.github.com>
2024-07-30 01:28:31 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,6 +1,3 @@
-#(©)Codexbotz
-#Recoded By @Its_Tartaglia_Childe
-
 from pyrogram import __version__
 from bot import Bot
 from config import OWNER_ID
@@ -8,19 +5,33 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
+    """
+    Handles callback queries for the bot.
+
+    Args:
+        client (Bot): The Pyrogram bot instance.
+        query (CallbackQuery): The callback query object.
+
+    Returns:
+        None
+    """
     data = query.data
+
+    # Handle "about" callback data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>┏━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┓\n× ○ ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ : <a href='tg://user?id=5437374877'>ᴀʙʀᴀʜᴀᴍ™ [𝕃𝕠𝕤𝕥]</a>\n× ○ ᴀɴɪᴍᴇ ᴄʜᴀᴛ : <a href='https://t.me/+04k5y7NJpdc0YTll'>ʜᴇɴᴛᴀɪ ꜰʟɪx :)</a>\n× ○ ᴄᴏᴍᴍᴜɴɪᴛʏ : <a href='https://t.me/Anime_Flix_Network'>ᴀɴɪᴍᴇ ꜰʟɪx ɴᴇᴛᴡᴏʀᴋ</a>\n┗━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┛</b>",
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
+            text=f"<b>┏━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┓\n× ○ ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ : <a href='tg://user?id=7102263732'>Waifu</a>\n× ○ ᴀɴɪᴍᴇ ᴄʜᴀᴛ : <a href='https://t.me/+ZsTyVaKlWL9lNjdl'>ʜᴇɴᴛᴀɪ ꜰʟɪx :)</a>\n× ○ ᴄᴏᴍᴍᴜɴɪᴛʏ : <a href='https://t.me/blackhawks_unit'>Haniflix network</a>\n┗━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┛</b>",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("☠️ ᴄʟᴏꜱᴇ ☠️", callback_data = "close")
+                        InlineKeyboardButton("☠️ ᴄʟᴏꜱᴇ ☠️", callback_data="close")
                     ]
                 ]
             )
         )
+
+    # Handle "close" callback data
     elif data == "close":
         await query.message.delete()
         try:
