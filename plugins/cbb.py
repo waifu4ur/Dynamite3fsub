fc8ea007cced5c6c3b0b621212d507352917724b
waifu4ur <153315802+waifu4ur@users.noreply.github.com>
2024-07-30 00:42:24 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,43 +1,29 @@
+#(©)Codexbotz
+#Recoded By @Its_Tartaglia_Childe
+
 from pyrogram import __version__
 from bot import Bot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-import logging
-
-logging.basicConfig(level=logging.INFO)
-
-def about_text():
-    """Returns the about text"""
-    return f"""
-    <b>┏━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┓
-    × ○ ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ : <a href='tg://user?id=7102263732'>Akki♛</a>
-    × ○ ᴀɴɪᴍᴇ ᴄʜᴀᴛ : <a href='https://t.me/+ZsTyVaKlWL9lNjdl'>Haniflix :)</a>
-    × ○ ᴄᴏᴍᴍᴜɴɪᴛʏ : <a href='https://t.me/blackhawks_unit'>Anivoid</a>
-    ┗━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┛</b>
-    """
-
-def about_keyboard():
-    """Returns the about keyboard"""
-    return InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton("☠️ ᴄʟᴏꜱᴇ ☠️", callback_data="close")
-            ]
-        ]
-    )
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text=about_text(),
-            disable_web_page_preview=True,
-            reply_markup=about_keyboard()
+            text = f"<b>┏━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┓\n× ○ ᴅᴇᴠᴇʟᴏᴘᴇᴅ ʙʏ : <a href='tg://user?id=5437374877'>ᴀʙʀᴀʜᴀᴍ™ [𝕃𝕠𝕤𝕥]</a>\n× ○ ᴀɴɪᴍᴇ ᴄʜᴀᴛ : <a href='https://t.me/+04k5y7NJpdc0YTll'>ʜᴇɴᴛᴀɪ ꜰʟɪx :)</a>\n× ○ ᴄᴏᴍᴍᴜɴɪᴛʏ : <a href='https://t.me/Anime_Flix_Network'>ᴀɴɪᴍᴇ ꜰʟɪx ɴᴇᴛᴡᴏʀᴋ</a>\n┗━━━━━•◦●◉✿ ❟❛❟ ✿◉●◦•━━━━━━┛</b>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("☠️ ᴄʟᴏꜱᴇ ☠️", callback_data = "close")
+                    ]
+                ]
+            )
         )
     elif data == "close":
         await query.message.delete()
         try:
             await query.message.reply_to_message.delete()
-        except Exception as e:
-            logging.error(f"Error deleting message: {e}")
+        except:
+            pass
